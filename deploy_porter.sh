27b06a6fed21b1bc60963a107bac139ddbc25156
Edward Shao <edward_shao@trend.com.tw>
2016-08-23 12:06:32 +0800

    add script to deploy porter image to SD

---
+++ deploy_porter.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+SRC_PATH=$PWD/gdp-src-build/tmp/deploy/images/porter
+TARGET_DEV="/dev/sdc"
+TARGET_DEV_PART=${TARGET_DEV}1
+MNT_POINT="/mnt"
+
+echo "mount SD..."
+sudo mount $TARGET_DEV_PART $MNT_POINT
+
+echo "clean SD..."
+sudo rm -rf $MNT_POINT/*
+
+echo "extact package to SD..."
+sudo tar --extract --numeric-owner --preserve-permissions --preserve-order --totals \
+	 --directory=$MNT_POINT --file=$SRC_PATH/genivi-dev-platform-porter.tar.bz2 
+sudo cp --remove-destination $SRC_PATH/uImage $SRC_PATH/uImage-r8a7791-porter.dtb $MNT_POINT/boot
+
+echo "sync..."
+sync
+
+echo "umount SD..."
+sudo umount $MNT_POINT
